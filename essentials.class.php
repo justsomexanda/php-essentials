934ebd68526789bfb5439d57dcea0548f5566c82
Alexander Blasl <48434066+justsomexanda@users.noreply.github.com>
2020-11-10 20:54:08 +0100

    Added progress_bar

--- essentials.class.php
+++ essentials.class.php
@@ -178,6 +178,13 @@ class Essentials
         }
         return $usertext;
     }
+    
+    function progress_bar($done, $total, $info = "", $width = 50)
+    {
+        $perc = round(($done * 100) / $total);
+        $bar = round(($width * $perc) / 100);
+        return sprintf("%s%%[%s>%s]%s\r", $perc, str_repeat("=", $bar), str_repeat(" ", $width - $bar), $info);
+    }
 }
 
 ?>
